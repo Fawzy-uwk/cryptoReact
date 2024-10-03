0aa21444482b6c055f17f9c1473529f05842d05f
Fawzy Mohamed <75136326+Fawzy-uwk@users.noreply.github.com>
2024-10-03 18:33:40 +0300

    🚪

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import Store from "./App/Store.js";
+import Store from "./App/Store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
