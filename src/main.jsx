25382f402683138ad439b5e038d980afebe294e7
Fawzy Mohamed <75136326+Fawzy-uwk@users.noreply.github.com>
2024-10-03 18:46:08 +0300

    🚯

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import Store from "./App/Store";
+import store from "./App/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <Provider store={Store}>
+      <Provider store={store}>
         <App />
       </Provider>
     </Router>
